3d2fac2d50edeb08adc9c2e2ab3fb53bbf52d464
GeekROS <15300555770>
2024-01-11 18:29:29 +0800

    1.0.0

--- framework/scenes/script/launch.gd
+++ framework/scenes/script/launch.gd
@@ -1,17 +1,28 @@
 extends Control
 
+# 初始化用户登录数据
+var login_mail: String = ""
+var login_password: String = ""
+
 func _ready():
-	pass
+	# 默认清空用户登录数据
+	login_mail = ""
+	login_password = ""
 
 func _process(_delta):
 	pass
 
 func _on_launch_sound_finished():
 	print("Launch Sound End")
+	# 确保背景音效循环播放
 	$LaunchSound.play()
-
 
 func _on_start_button_pressed():
 	print("Launch Login Start")
-	$LoginMain.visible = false
-	$LaunchSound.stop()
+	# 获取用户登录数据
+	login_mail = $Login/Main/LoginMail.text
+	login_password = $Login/Main/LoginPassword.text
+	# 校验用户登录数据
+	if login_mail != "" and login_password != "":
+		$Login/Main.visible = false
+		$Login/Progress.visible = true
