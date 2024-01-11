98346c8e4572e9f1939dfdb74c4f2b65ca6f8437
GeekROS <15300555770>
2024-01-11 20:49:23 +0800

    1.0.0

--- framework/script/launch/launch.gd
+++ framework/script/launch/launch.gd
@@ -13,16 +13,21 @@ func _process(_delta):
 	pass
 
 func _on_launch_sound_finished():
-	print("Launch Sound End")
 	# 确保背景音效循环播放
 	$LaunchSound.play()
 
 func _on_start_button_pressed():
-	print("Launch Login Start")
 	# 获取用户登录数据
 	login_mail = $Login/Main/LoginMail.text
 	login_password = $Login/Main/LoginPassword.text
 	# 校验用户登录数据
 	if login_mail != "" and login_password != "":
+		$Login.visible = false
+		$Server.visible = true
 		$Login/Main.visible = false
-		$Login/Progress.visible = true
+
+func _on_item_pressed():
+	$Server.visible = false
+	$Login.visible = true
+	$Login/Main.visible = false
+	$Login/Progress.visible = true
