b85186c5c8caa5d177829f32cce1c22b24e842c9
GeekROS <15300555770>
2024-01-14 02:27:30 +0800

    1.0.0

--- framework/scripts/world/player/player.gd
+++ framework/scripts/world/player/player.gd
@@ -25,51 +25,9 @@ func _process(_delta):
 			if speed > 0:
 				# 获取玩家的位置
 				var player_position = position
-				var mouse_position = get_global_mouse_position()
-				# 计算方向向量
-				var relative_mouse_position = mouse_position - player_position
-				var angle = atan2(relative_mouse_position.y, relative_mouse_position.x)
-				# 将角度转换为八个方向之一
-				if angle >= -PI/8 and angle < PI/8: # 右
-					Input.action_release("ui_up")
-					Input.action_release("ui_down")
-					Input.action_release("ui_left")
-					Input.action_press("ui_right")
-				elif angle >= PI/8 and angle < 3*PI/8: # 右下
-					Input.action_release("ui_up")
-					Input.action_press("ui_down")
-					Input.action_release("ui_left")
-					Input.action_press("ui_right")
-				elif angle >= 3*PI/8 and angle < 5*PI/8: # 下
-					Input.action_release("ui_up")
-					Input.action_press("ui_down")
-					Input.action_release("ui_left")
-					Input.action_release("ui_right")
-				elif angle >= 5*PI/8 and angle < 7*PI/8: # 左下
-					Input.action_release("ui_up")
-					Input.action_press("ui_down")
-					Input.action_press("ui_left")
-					Input.action_release("ui_right")
-				elif angle >= -7*PI/8 and angle < -5*PI/8: # 左上
-					Input.action_press("ui_up")
-					Input.action_release("ui_down")
-					Input.action_press("ui_left")
-					Input.action_release("ui_right")
-				elif angle >= -5*PI/8 and angle < -3*PI/8: # 上
-					Input.action_press("ui_up")
-					Input.action_release("ui_down")
-					Input.action_release("ui_left")
-					Input.action_release("ui_right")
-				elif angle >= -3*PI/8 and angle < -PI/8: # 右上
-					Input.action_press("ui_up")
-					Input.action_release("ui_down")
-					Input.action_release("ui_left")
-					Input.action_press("ui_right")
-				else: # 左
-					Input.action_release("ui_up")
-					Input.action_release("ui_down")
-					Input.action_press("ui_left")
-					Input.action_release("ui_right")
-				var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-				velocity = direction * speed
-				move_and_slide()
+				var mouse_position = get_local_mouse_position()
+				var angle = snapped(mouse_position.angle(), PI/4) / (PI/4)
+				angle = wrapi(int(angle), 0, 8)
+				if mouse_position.length() > 10:
+					velocity = mouse_position.normalized() * speed
+					move_and_slide()
