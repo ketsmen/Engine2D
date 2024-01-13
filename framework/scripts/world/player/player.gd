24af8cc4179ae90799299fc51bd34c9560a847fe
GeekROS <15300555770>
2024-01-14 03:17:14 +0800

    1.0.0

--- framework/scripts/world/player/player.gd
+++ framework/scripts/world/player/player.gd
@@ -16,18 +16,30 @@ func _process(_delta):
 		var viewport_mouse_position = get_viewport().get_mouse_position()
 		# 默认速度
 		var speed = 0
+		# 默认步长，将影响玩家最终的走位
+		var step_length = 10
 		# 检查鼠标是否在窗口内
 		if viewport_rect.has_point(viewport_mouse_position):
 			if Input.is_action_pressed("walking"):
-				speed = 45
+				# 鼠标左键行走
+				speed = 40
 			if Input.is_action_pressed("running"):
+				# 鼠标左键奔跑
 				speed = 80
 			if speed > 0:
 				# 获取玩家的位置
 				var player_position = position
+				# 获取鼠标的位置
 				var mouse_position = get_local_mouse_position()
+				# 获取鼠标的角度(以弧度为单位)并捕捉最接近45度的倍数(0,45,90,135,180,-135,-90,-45)
 				var angle = snapped(mouse_position.angle(), PI/4) / (PI/4)
+				# 将获取的倍数分别除以45度(0,1,2,3,4,-3,-2,-1)
 				angle = wrapi(int(angle), 0, 8)
-				if mouse_position.length() > 10:
-					velocity = mouse_position.normalized() * speed
+				# 鼠标位置距离玩家多远才触发
+				if mouse_position.length() > 1:
+					var direction = mouse_position.normalized()
+					# TODO 需要增加步长控制，将影响玩家最终的走位
+					#var new_position = player_position + direction * step_length
+					#position = new_position
+					velocity = direction * speed
 					move_and_slide()
