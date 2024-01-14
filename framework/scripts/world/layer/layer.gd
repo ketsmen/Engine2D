adf4b4252848420a4f147cb68ec200741e2c0586
GeekROS <15300555770>
2024-01-14 22:00:21 +0800

    1.0.0

--- framework/scripts/world/layer/layer.gd
+++ framework/scripts/world/layer/layer.gd
@@ -8,7 +8,9 @@ extends CanvasLayer
 @onready var location = $MiniMap/Main/Show/Location
 
 func _process(_delta):
+	# 相机位置同步
 	camera.position = owner.find_child("Player").position
+	# 人物位置同步 TODO 待人物动画确定后需要计算人物偏移量
 	location.position = owner.find_child("Player").position
 	# TODO 显示周边人物、NPC、怪物
 	
