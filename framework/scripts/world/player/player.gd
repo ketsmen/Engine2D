6f74ff3c9e0571072c8667793e5a80b789b69d48
GeekROS <15300555770>
2024-01-19 00:51:18 +0800

    1.0.0

--- framework/scripts/world/player/player.gd
+++ framework/scripts/world/player/player.gd
@@ -114,12 +114,12 @@ func _physics_process(_delta):
 		# 检查鼠标是否在窗口内
 		if viewport_rect.has_point(viewport_mouse_position):
 			# 获取玩家的位置
-			var player_position = global_position
+			var player_position = position
 			# 获取鼠标的位置
-			var mouse_position = get_global_mouse_position()
-			var that_position = mouse_position - player_position
+			var mouse_position = get_local_mouse_position()
+			# var that_position = mouse_position - player_position
 			# 获取鼠标的角度(以弧度为单位)并捕捉最接近45度的倍数(0,1,2,3,4,-4,-3,-2,-1)
-			var angle = snapped(that_position.angle(), PI/4) / (PI/4)
+			var angle = snapped(mouse_position.angle(), PI/4) / (PI/4)
 			# 将获取的倍数分别除以45度(0,1,2,3,4,5,6,7)
 			player_angle = wrapi(int(angle), 0, 8)
 			# 计算方向向量
@@ -134,7 +134,7 @@ func _physics_process(_delta):
 			if player_angle == 7:
 				direction = Vector2(0.7, -0.4)
 			on_switch_layer()
-			# 鼠标左右键按下时触发
+			# 鼠标左按下时触发
 			if Input.is_action_pressed("walking") or Input.is_action_pressed("running"):
 				# 更新速度和动作
 				if Input.is_action_pressed("walking"):
@@ -149,10 +149,14 @@ func _physics_process(_delta):
 					on_sound_play(load("res://framework/statics/musics/running.wav"))
 				if player_action_speed > 0:
 					# 鼠标位置距离玩家多远才触发
-					if mouse_position.length() > 10:
+					if mouse_position.length() > 30:
 						player_body.get_child(0).animation = str(player_angle) + "_" + player_action
-						player_body.get_child(1).animation = str(player_angle) + "_" + player_action
-						player_body.get_child(2).animation = str(player_angle) + "_" + player_action
+						var weapon_id = Global.get_player_weapon_value()
+						if weapon_id != "000":
+							player_body.get_child(1).animation = str(player_angle) + "_" + player_action
+						var wing_id = Global.get_player_wing_value()
+						if wing_id != "000":
+							player_body.get_child(2).animation = str(player_angle) + "_" + player_action
 						velocity = direction.normalized() * player_action_speed
 						move_and_slide()
 			if Input.is_action_just_released("walking") or Input.is_action_just_released("running"):
@@ -174,8 +178,12 @@ func on_action_stop():
 	velocity = Vector2.ZERO
 	player_action = "stand"
 	player_body.get_child(0).animation = str(player_angle) + "_" + player_action
-	player_body.get_child(1).animation = str(player_angle) + "_" + player_action
-	player_body.get_child(2).animation = str(player_angle) + "_" + player_action
+	var weapon_id = Global.get_player_weapon_value()
+	if weapon_id != "000":
+		player_body.get_child(1).animation = str(player_angle) + "_" + player_action
+	var wing_id = Global.get_player_wing_value()
+	if wing_id != "000":
+		player_body.get_child(2).animation = str(player_angle) + "_" + player_action
 
 func on_sound_play(sound_file):
 	if sound.stream != sound_file:
